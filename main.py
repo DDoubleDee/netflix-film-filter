ecd2a6ffd2812a3b9bf44c7342a72b49f43c41c3
DDoubleDee <91531619+DDoubleDee@users.noreply.github.com>
2021-09-28 21:19:13 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -7,17 +7,27 @@ from sortdate import sortdate
 from searchengine import search
 from csvtosql import csvtosql
 
-engine = create_engine(dbconn())
-csvtosql(engine)  # Check for table, if doesn't exist, write a new one
-usertype = 0  # user input. 0 = No filter, 1 = Movie, 2 = TV Show
-mlist = typefilter(engine, usertype)  # Filtering by type
+#engine = create_engine(dbconn())
+#csvtosql(engine)  # Check for table, if doesn't exist, write a new one
+usertype = 0  # user input. 0 = No filter, 1 = Movie, 2 = TV Show.
+#mlist = typefilter(engine, usertype)  # Filtering by type and forming the list for further manipulations
 userdate = 0  # user input. 0 = unsorted, 1 = condescending, 2 = ascending
-mlist = sortdate(mlist, userdate)  # Sorting by release date
-usersearch = 'transformers'  # user input. Search bar contents
+#mlist = sortdate(mlist, userdate)  # Sorting by release date
+usersearch = 'transformers'  # user input. Search bar contents. If empty = do nothing
 s1 = True  # Search by title /user input. All of these are toggles
 s2 = True  # Search by country
 s3 = True  # Search by genre
-s4 = True  # Actor search
-s5 = True  # Description search
-mlist = search(mlist, usersearch, s1, s2, s3, s4, s5)
-print(mlist)
+s4 = True  # Search by actor
+s5 = True  # Search by description
+#if usersearch != '':
+    #mlist = search(mlist, usersearch, s1, s2, s3, s4, s5)  # Execute search
+#output = mlist
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello world!"
+@app.route("/test")
+def test():
+    return "TEST"
+if __name__ == "__main__":
+    app.run(debug=True)
